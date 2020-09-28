ba7717f400b7d4d454a48be69f3af05bb7572f18
Aidan Lane <aidanlane0@protonmail.com>
2020-09-28 19:06:17 -0400

    Initial Commit

---
+++ Flixter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixter
+//
+//  Created by Aidan Lane on 9/28/20.
+//  Copyright © 2020 Aidan Lane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
