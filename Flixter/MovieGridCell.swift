7c1c1f20c98b191923b27a66a6051385efff4d9f
Aidan Lane <aidanlane0@protonmail.com>
2020-10-07 23:10:27 -0400

    added more views

---
+++ Flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Aidan Lane on 10/7/20.
+//  Copyright © 2020 Aidan Lane. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIView!
+}
